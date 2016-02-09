6b168924c2915790b7cbdfce1932cb051d56d946
Eduard Alfonso <xsynergy510x@gmail.com>
2016-02-08 18:20:59 -0800

    Revert "settings: Add preference for automagic LED colors"
    This reverts commit a50a3f3de91258d2eae0d3282301f17dff65ccf9.
    
    Conflicts:
    	src/com/android/settings/notificationlight/NotificationLightSettings.java

--- src/com/android/settings/notificationlight/NotificationLightSettings.java
+++ src/com/android/settings/notificationlight/NotificationLightSettings.java
@@ -26,7 +26,6 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
@@ -34,7 +33,6 @@ import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -56,7 +54,6 @@ import java.util.List;
 import java.util.Map;
 
 import cyanogenmod.providers.CMSettings;
-import cyanogenmod.util.ColorUtils;
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
 
 public class NotificationLightSettings extends SettingsPreferenceFragment implements
@@ -80,7 +77,6 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
     private CMSystemSettingSwitchPreference mCustomEnabledPref;
     private CMSystemSettingSwitchPreference mMultipleLedsEnabledPref;
     private CMSystemSettingSwitchPreference mScreenOnLightsPref;
-    private CMSystemSettingSwitchPreference mAutoMagic;
     private ApplicationLightPreference mDefaultPref;
     private ApplicationLightPreference mCallPref;
     private ApplicationLightPreference mVoicemailPref;
@@ -114,10 +110,6 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
 
         mDefaultPref = (ApplicationLightPreference) findPreference(DEFAULT_PREF);
         mDefaultPref.setOnPreferenceChangeListener(this);
-
-        mAutoMagic = (CMSystemSettingSwitchPreference)
-                findPreference(CMSettings.System.NOTIFICATION_LIGHT_COLOR_AUTO);
-        mAutoMagic.setOnPreferenceChangeListener(this);
 
         // Advanced light settings
         mNotificationLedBrightnessPref = (PreferenceScreen)
@@ -258,9 +250,6 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
             return;
         }
 
-        boolean autoMagic = CMSettings.System.getInt(getContentResolver(),
-                CMSettings.System.NOTIFICATION_LIGHT_COLOR_AUTO, 1) == 1;
-
         // Add the Application Preferences
         if (mApplicationPrefList != null) {
             mApplicationPrefList.removeAll();
@@ -286,27 +275,10 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         }
     }
 
-    private int getInitialColorForPackage(String packageName) {
-        boolean autoMagic = CMSettings.System.getInt(getContentResolver(),
-                CMSettings.System.NOTIFICATION_LIGHT_COLOR_AUTO, 1) == 1;
-        int color = mDefaultColor;
-        if (autoMagic) {
-            try {
-                Drawable icon = mPackageManager.getApplicationIcon(packageName);
-                color = ColorUtils.generateAlertColorFromDrawable(icon);
-            } catch (NameNotFoundException e) {
-                Log.e("NOTIF", "package not found! " + packageName);
-            }
-        }
-        Log.d("NOTIF", "pkg=" + packageName + " color=" + color + " default=" + mDefaultColor + " magic=" + autoMagic);
-        return color;
-    }
-
     private void addCustomApplicationPref(String packageName) {
         Package pkg = mPackages.get(packageName);
         if (pkg == null) {
-            int color = getInitialColorForPackage(packageName);
-            pkg = new Package(packageName, color, mDefaultLedOn, mDefaultLedOff);
+            pkg = new Package(packageName, mDefaultColor, mDefaultLedOn, mDefaultLedOff);
             mPackages.put(packageName, pkg);
             savePackageList(false);
             refreshCustomApplicationPrefs();
@@ -439,8 +411,7 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         if (preference == mEnabledPref || preference == mCustomEnabledPref ||
                 preference == mMultipleLedsEnabledPref ||
                 preference == mNotificationLedBrightnessPref ||
-                preference == mScreenOnLightsPref ||
-                preference == mAutoMagic) {
+                preference == mScreenOnLightsPref) {
             getActivity().invalidateOptionsMenu();
         } else {
             ApplicationLightPreference lightPref = (ApplicationLightPreference) preference;
